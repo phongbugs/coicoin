8c56be87043c4a640e501d58ece1bbbe5d9cb7e2
nguyenduyphongtn <nguyenduyphongtn@gmail.com>
2021-07-14 00:11:28 +0700

    fix home page

--- web/src/App.js
+++ web/src/App.js
@@ -1,38 +1,75 @@
 import React from 'react';
 import './App.css';
-import ComboBox from './CoinManagement';
-import { Container, Row, Col } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import TextField from '@material-ui/core/TextField';
+import CoinComboBox from './CoinComboBox';
+import CoinTextField from './CoinTextField';
+import Grid from '@material-ui/core/Grid';
+import AddIcon from '@material-ui/icons/Add';
+import Button from '@material-ui/core/Button';
+import CoinList from './CoinList';
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    textAlign: 'center',
+    padding: '10px',
+  },
+  input: {
+    color: 'white',
+  },
+}));
 function App() {
+  const classes = useStyles();
   return (
-    <div class='App'>
-      <Container fluid>
-        <Row>
-          <Col xs={12} md={4}>
-            <ComboBox></ComboBox>
-          </Col>
-          <Col xs={6} md={4}>
-            <TextField
-              id='outlined-basic'
-              label='Số lượng'
-              variant='outlined'
-            />
-          </Col>
-          <Col xs={6} md={4}>
-            <TextField
-              id='outlined-number'
-              label='Number'
-              type='number'
-              InputLabelProps={{
-                shrink: true,
-              }}
-              variant='outlined'
-            />
-          </Col>
-        </Row>
-        <Row></Row>
-      </Container>
+    <div className={classes.root}>
+      <Grid container spacing={1}>
+        <Grid item xs={12} sm={3} md={3} lg={3} xl={3}>
+          <CoinComboBox InputProps={{
+              className: classes.input,
+            }}/>
+        </Grid>
+        <Grid item xs={4} sm={3} md={3} lg={3} xl={3}>
+          <CoinTextField
+            size='small'
+            id='txtQuantity'
+            label='Số lượng'
+            type='number'
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant='outlined'
+            InputProps={{
+              className: classes.input,
+            }}
+          />
+        </Grid>
+        <Grid item xs={4} sm={3} md={3} lg={3} xl={3}>
+          <CoinTextField
+            size='small'
+            id='txtFund'
+            label='Vốn'
+            type='number'
+            InputLabelProps={{
+              shrink: true,
+            }}
+            variant='outlined'
+            InputProps={{
+              className: classes.input,
+            }}
+          />
+        </Grid>
+        <Grid item xs={2} sm={3} md={3} lg={3} xl={3}>
+          <Button
+            variant='contained'
+            className={classes.button}
+            startIcon={<AddIcon />}
+          >
+            Add
+          </Button>
+        </Grid>
+        <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+            <CoinList />
+        </Grid>
+      </Grid>
     </div>
   );
 }
