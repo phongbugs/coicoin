f113df5d8a82f730de321cefc4ee7f6af81cf74b
nguyenduyphongtn <nguyenduyphongtn@gmail.com>
2021-08-04 22:31:01 +0700

    add spinner add icon and fix some bugs

--- web/src/App.js
+++ web/src/App.js
@@ -4,6 +4,7 @@ import CoinComboBox from './components/CoinComboBox';
 import CoinTextField from './components/CoinTextField';
 import Grid from '@material-ui/core/Grid';
 import AddIcon from '@material-ui/icons/Add';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Button from '@material-ui/core/Button';
 import CoinList from './components/CoinList';
 import PairComboBox from './components/PairComboBox';
@@ -30,48 +31,65 @@ const log = console.log;
 
 const fetchPrice = async (market) => {
   let url = process.env.REACT_APP_API_URL;
-  const response = await fetch(url + 'info/price/' + market);
-  const price = await response.json();
-  return +price;
+  try {
+    const response = await fetch(url + 'info/price/' + market);
+    const price = await response.json();
+    return +price;
+  } catch (error) {
+    log(error);
+    alert('API Service error');
+    throw error;
+  }
 };
 const fetchPrices = async (markets) => {
   let url = process.env.REACT_APP_API_URL;
-  const response = await fetch(url + 'info/prices/' + markets.toString());
-  const prices = await response.json();
-  return prices;
+  try {
+    const response = await fetch(url + 'info/prices/' + markets.toString());
+    const prices = await response.json();
+    return prices;
+  } catch (error) {
+    log(error);
+    alert(error);
+  }
 };
 const getCoinsWithNewPrices = async (coins) => {
   let prices = await fetchPrices(coins.map((coin) => coin.s + coin.p));
   return coins.map((coin) => {
     let market = coin.s + coin.p;
-    let c = { ...coin, cf: coin.q * prices[market] };
-    return c
+    let currentFund = coin.cf;
+    let price = prices[market];
+    if (price) currentFund = coin.q * price;
+    let c = { ...coin, cf: currentFund };
+    return c;
   });
 };
-function delay(second) {
-  return new Promise((res) => setTimeout(res, second * 1000));
-}
-const runUpdatePrices = async (dispatch, entities) => {
-  log('coicoin')
-  dispatch(updateCoins(await getCoinsWithNewPrices(entities)));
-  await delay(2);
-  await runUpdatePrices(dispatch, entities);
-};
+// function delay(second) {
+//   return new Promise((res) => setTimeout(res, second * 1000));
+// }
+// const runUpdatePrices = async (dispatch, entities) => {
+//   dispatch(updateCoins(await getCoinsWithNewPrices(entities)));
+//   await delay(2);
+//   await runUpdatePrices(dispatch, entities);
+// };
 function App() {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const [isAdding, setIsAdding] = useState(false);
   const [quantityCoin, setQuantityCoin] = useState('');
   const [originalFund, setOriginalFund] = useState('');
   const [symbolCoin, setSymbolCoin] = useState('');
   const [symbolCoinPair, setSymbolCoinPair] = useState('USDT');
-  const { currentState } = useSelector(
+  let { currentState } = useSelector(
     (state) => ({ currentState: state.coin }),
     shallowEqual
   );
   useEffect(() => {
-    // setTimeout(() => log(currentState), 10000);
-    // log(currentState);
-    runUpdatePrices(dispatch, currentState.entities);
+    //runUpdatePrices(dispatch, currentState.entities);
+    //dispatch(updateCoins(await getCoinsWithNewPrices(currentState.entities)));
+    // async function updatePrices() {
+    //   await getCoinsWithNewPrices(currentState.entities);
+    // }
+    // updatePrices();
   }, []);
   const sendSymbol = (symbol) => {
     console.log(symbol);
@@ -102,7 +120,8 @@ function App() {
         coin['q'] = +quantityCoin;
         coin['of'] = +originalFund;
         // fetch price
-        let market = symbolCoin + symbolCoinPair;
+        let market =
+          (symbolCoin === 'SMN' ? 'SAFEMOON' : symbolCoin) + symbolCoinPair;
         let price = await fetchPrice(market);
         coin['cf'] = price * +quantityCoin;
       }
@@ -174,9 +193,18 @@ function App() {
               fontWeight: 'bold',
             }}
             variant='contained'
-            //className={{ button: classes.button }}
-            startIcon={<AddIcon />}
-            onClick={async () => dispatch(addCoin(await getCoin(symbolCoin)))}
+            startIcon={
+              isAdding ? (
+                <CircularProgress size={20} style={{ color: '#fff' }} />
+              ) : (
+                <AddIcon />
+              )
+            }
+            onClick={async () => {
+              setIsAdding(true);
+              dispatch(addCoin(await getCoin(symbolCoin)));
+              setIsAdding(false);
+            }}
           >
             Thêm
           </Button>
@@ -194,10 +222,20 @@ function App() {
               fontWeight: 'bold',
             }}
             variant='contained'
-            startIcon={<AddIcon />}
-            onClick={async () =>
-              dispatch(updateCoins(await getCoinsWithNewPrices(currentState.entities)))
+            startIcon={
+              isAdding ? (
+                <CircularProgress size={20} style={{ color: '#fff' }} />
+              ) : (
+                <AddIcon />
+              )
             }
+            onClick={async () => {
+              dispatch(
+                updateCoins(await getCoinsWithNewPrices(currentState.entities))
+              );
+              //runUpdatePrices(dispatch, currentState.entities);
+              log(currentState);
+            }}
           >
             Thêm
           </Button>
