a2ed611613ca2f82fb0ff4d2b1a62b53423e1fd7
nguyenduyphongtn <nguyenduyphongtn@gmail.com>
2021-08-17 06:30:34 +0700

    change localApiUrl

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   APIKEY: 'UMbi3qd7GRH128Be3dcl4pPhiProD5ajYOULcWnyxtOs3gcujNVcKzfaK2DMfAqo',
   APISECRET: 'DUA3aKL7cKno5AYvGHdWvICzQfsTJMzz0PqOWnAF64BmaBUKiEwJwias9STGWFhO',
   development: {
-    localApiUrl: 'http://192.168.2.185:9999',
+    localApiUrl: 'http://localhost:9999',
     TIMEOUT_SYNC_BNBMARKET: 10000,
     TIMEOUT_SYNC_EXTRAMARKET: 10000,
     TIMEOUT_START_INIT_SYMBOLS: 30000,
