7cef7d1d610a210fe1587b937c873cf3f55399bb
nguyenduyphongtn <nguyenduyphongtn@gmail.com>
2021-10-25 23:04:50 +0700

    BUILD

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports = {
   APISECRET: 'DUA3aKL7cKno5AYvGHdWvICzQfsTJMzz0PqOWnAF64BmaBUKiEwJwias9STGWFhO',
   development: {
     localApiUrl: 'http://localhost:9999',
-    TIMEOUT_SYNC_BNBMARKET: 30000,
-    TIMEOUT_SYNC_EXTRAMARKET: 60000,
+    TIMEOUT_SYNC_BNBMARKET: 20000,
+    TIMEOUT_SYNC_EXTRAMARKET: 30000,
     TIMEOUT_START_INIT_SYMBOLS: 10000,
   },
   production: {
